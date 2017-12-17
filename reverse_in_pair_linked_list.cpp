810b2075f8a2ca1bba0b1132f8a91fce25ddcb97
Divyansh Chowdhary <divyanshchowdhary96@gmail.com>
2017-12-17 23:41:36 +0530

    Update and rename reverse_in_pair_linked_list_2.cpp to reverse_in_pair_linked_list.cpp

--- reverse_in_pair_linked_list.cpp
+++ reverse_in_pair_linked_list.cpp
@@ -1,3 +1,5 @@
+//Reverse the linked list in pairs. If you have a linked list that holds 1 → 2 → 3 → 4 →-X, then after the function has been called the linked list would hold 2 → 1 → 4 → 3 → X.
+
 #include<iostream>
 using namespace std;
 struct node{
